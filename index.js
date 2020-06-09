0f4d61d5ebe4cbbc5cd7c9d610cda89a9150e706
Anc <anc136k@gmail.com>
2020-06-09 16:07:29 +0800

    feat:add BarChart

--- index.js
+++ index.js
@@ -23,16 +23,30 @@ https.get('https://codestats.net/api/users/' + CODESTATS_USER, (resp) => {
 	let res = JSON.parse(data);
 	let level = (LEVEL_FACTOR * Math.sqrt(res.total_xp)).toFixed(2);
 
-	let content = "User:" + res.user + "\n" +
-			"Level:" + level + "\n" + 
-			"New_xp:" + res.new_xp + "\n" +
-			"-------------" + "\n" +
+	var languages = res.languages;
+	var lang_content = "";
+	Object.keys(languages).forEach(function(key){
+		var xps = languages[key].new_xps;
+		if(xps >= 100){
+			var txt = key;
+			if(key.length > 11){
+				txt = key.split(" ")[0];	
+			}	
+			// TODO:xp level
+			lang_content += txt.padEnd(11) + generateBarChart(languages[key].new_xps / 50, 21) + "5000/"  + languages[key].new_xps + "\n";
+		}
+	});
+	let content = 
+			"Level".padEnd(11) + generateBarChart(level,21) + "100/" + level + "\n" + 
+			"Coding".padEnd(11) + generateBarChart(res.new_xp / 50, 21) + "5000/" + res.new_xp + "\n" +
+			lang_content +
+			"----------------------------------------" + "\n" +
 			"https://codestats.net/users/" + CODESTATS_USER ;
 
     console.log(content);
 
 	box.update({
-	  filename: '📊 Yesterday Codestats',
+      filename: '📊 Recent Code::Stats',
 	  description: 'codestats analysis',
 	  content: content
 	}).catch(
@@ -44,4 +58,18 @@ https.get('https://codestats.net/api/users/' + CODESTATS_USER, (resp) => {
 	throw Error(err)
 });
 
+function generateBarChart(percent, size) {
+      const syms = "░▏▎▍▌▋▊▉█";
+
+      const frac = Math.floor((size * 8 * percent) / 100);
+      const barsFull = Math.floor(frac / 8);
+      if (barsFull >= size) {
+              return syms.substring(8, 9).repeat(size);
+            }
+      const semi = frac % 8;
+
+      return [syms.substring(8, 9).repeat(barsFull), syms.substring(semi, semi + 1)]
+        .join("")
+        .padEnd(size, syms.substring(0, 1));
+}
 
